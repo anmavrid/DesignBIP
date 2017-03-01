27f647d29dbc878e656f8a4406ff61327093fc13
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2017-03-01 11:24:04 -0600

    WIP Add requirejs path and deactivate AR

--- config/config.default.js
+++ config/config.default.js
@@ -7,6 +7,12 @@ var config = require('./config.webgme'),
 // config.server.port = 8080;
 // config.mongo.uri = 'mongodb://127.0.0.1:27017/webgme_my_app';
 
-config.visualization.svgDirs = ["./src/svgs"];
+config.client.defaultConnectionRouter = 'basic2';
+
+config.requirejsPaths['widgets/DiagramDesigner'] =
+    './node_modules/diagram-designers/src/visualizers/widgets/DiagramDesigner';
+
+config.visualization.svgDirs = ['./src/svgs'];
+
 validateConfig(config);
 module.exports = config;
